7538abd1d14e3dd22b91c660d39ef7712e5807ed
Ruth Newman <ruthnewman@gmail.com>
2019-08-16 02:19:14 +0100

    add and remove note with title and body

--- src/index.js
+++ src/index.js
@@ -5,27 +5,40 @@ import * as serviceWorker from "./serviceWorker";
 const NoteApp = () => {
   const [notes, setNotes] = useState([]);
   const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
   const addNote = event => {
     event.preventDefault();
-    setNotes([...notes, { title }]);
+    setNotes([...notes, { title, body }]);
     setTitle("");
+  };
+
+  const removeNote = title => {
+    setNotes(notes.filter(note => note.title !== title));
   };
 
   return (
     <div>
       <h1>Notes</h1>
       {notes.map(note => (
-        <div>
-          <h3>{note.title}</h3>
+        <div key={note.title}>
+          <h4>{note.title} </h4>
+          <p>{note.body}</p>
+          <button onClick={() => removeNote(note.title)}> X</button>
         </div>
       ))}
       <p>Add a note</p>
       <form onSubmit={addNote}>
         <input
+          placeholder="Title"
           type="text"
           value={title}
           onChange={event => setTitle(event.target.value)}
+        />
+        <textarea
+          value={body}
+          placeholder="Add your note"
+          onChange={event => setBody(event.target.value)}
         />
         <button>Submit</button>
       </form>
